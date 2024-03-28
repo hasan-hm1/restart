2bb63fdc3af0f8a29046493efe70b46a08d46a81
Hasan Mohammed <hasanmohammed.hm1@gmail.com>
2024-03-28 10:57:57 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Hasan on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
